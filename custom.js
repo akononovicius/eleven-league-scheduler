5599ebda5c88c8d59d0bdd67db138ac7ea7b1315
Aleksejus Kononovicius <aleksejus.kononovicius@gmail.com>
2023-01-26 20:24:05 +0200

    improve custom league shuffling

--- custom.js
+++ custom.js
@@ -82,37 +82,25 @@
             };
             league.teams = league.teams.map((v, i) => {
                 let name = document.getElementById(`team-name-${i}`).value;
-                let type = document.getElementById(`team-type-${i}`).value;
-                let div = document.getElementById(`team-div-${i}`).value;
+                let type = parseInt(
+                    document.getElementById(`team-type-${i}`).value
+                );
+                let div = parseInt(
+                    document.getElementById(`team-div-${i}`).value
+                );
                 return {
                     name: name.length > 0 ? name : v.name,
                     type: type,
                     division: div,
                 };
             });
-            let lower_list = league.teams.slice(0, 4);
-            let mid_list = league.teams.slice(4, 8);
-            let upper_list = [];
-            if (league.n_teams > 8) {
-                upper_list = league.teams.slice(8, 12);
-                upper_list = upper_list.slice(0, -league.n_humans);
-            } else {
-                mid_list = mid_list.slice(0, -league.n_humans);
+            let team_list = [];
+            for (let div_id = 4; div_id >= 0; div_id = div_id - 1) {
+                let cur_list = league.teams.filter((v) => v.division == div_id);
+                shuffle(cur_list);
+                team_list = [...team_list, ...cur_list];
             }
-            let human_list = league.teams.slice(-league.n_humans);
-            shuffle(lower_list);
-            if (mid_list.length > 0) {
-                shuffle(mid_list);
-            }
-            if (upper_list.length > 0) {
-                shuffle(upper_list);
-            }
-            league.teams = [
-                ...lower_list,
-                ...mid_list,
-                ...upper_list,
-                ...human_list,
-            ];
+            league.teams = team_list.slice();
             league.ready = true;
             refresh_flag = true;
             switch_card("schedule-card");
